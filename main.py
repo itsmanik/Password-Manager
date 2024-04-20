249d8c731d339f249de243d18a1f8fa51ec11804
manik <manik.hilapure@gmail.com>
2024-04-21 00:45:28 +0530

    removed the extra file layout and integrated it into main file

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import tkinter
-import layout
+import save
 
 windows = tkinter.Tk()
 windows.minsize(400, 400)
 
-layout = layout.Layout()
+title_label = tkinter.Label(text="Password Manager", font=(20))
+title_label.pack()
+
+platform_label = tkinter.Label(text="Platform: ")
+platform_label.pack()
+platform = tkinter.Entry()
+platform.pack()
+
+email_label = tkinter.Label(text="E-mail: ")
+email_label.pack()
+email = tkinter.Entry()
+email.pack()
+
+password_label = tkinter.Label(text="Password: ")
+password_label.pack()
+password = tkinter.Entry()
+password.pack()
+
+save_button = tkinter.Button(text="Save", command=lambda: save.save(platform.get(), email.get(), password.get()))
+save_button.pack()
 
 windows.mainloop()
