8ea807877c92229b04eae9379f786a630ef893b0
manik <manik.hilapure@gmail.com>
2024-04-21 15:02:13 +0530

    bug fix, create file if it does not exists

--- save.py
+++ save.py
@@ -6,9 +6,14 @@ def save(platform, email, password):
         tkinter.messagebox.showerror(title="Error", message="Please specify platform and password")
         return
     item = {platform: {"email": email, "password": password}}
-    with open("data.json", "r") as password_data:
-        passwords = json.load(password_data)
-        passwords.update(item)
-    
+    try:
+        with open("data.json", "r") as password_data:
+            passwords = json.load(password_data)
+            passwords.update(item)
+    except FileNotFoundError:
+        with open("data.json", "w") as file:
+            passwords = {}
+            passwords.update(item)
+
     with open("data.json", "w") as password_data:
         json.dump(passwords, password_data, indent=4)
