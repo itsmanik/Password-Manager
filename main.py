78223ab4d1310686a356dca65a7a099c4f5cf035
manik <manik.hilapure@gmail.com>
2024-04-21 11:53:00 +0530

    added styling

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import tkinter
 import save
 
-windows = tkinter.Tk()
-windows.minsize(400, 400)
+window = tkinter.Tk()
+window.title("Password Manager")
+window.config(bg="#E1E9EF", padx=100, pady=50)
 
-title_label = tkinter.Label(text="Password Manager", font=(20))
-title_label.pack()
+title_label = tkinter.Label(text="Password Manager", font=("", 25, "bold"), fg="#161616", bg="#E1E9EF")
+title_label.grid(row=1, column=1, columnspan=2, pady=(0, 40))
 
-platform_label = tkinter.Label(text="Platform: ")
-platform_label.pack()
-platform = tkinter.Entry()
-platform.pack()
+platform_label = tkinter.Label(text="Platform", bg="#E1E9EF", font=("", 12, "normal")) 
+platform_label.grid(row=2, column=1, sticky="W")
+platform = tkinter.Entry(font=("", 15, "normal"))
+platform.grid(row=3, column=1, pady=(0, 20), padx=(0, 50))
 
-email_label = tkinter.Label(text="E-mail: ")
-email_label.pack()
-email = tkinter.Entry()
-email.pack()
+email_label = tkinter.Label(text="E-mail", bg="#E1E9EF", font=("", 12, "normal"))
+email_label.grid(row=2, column=2, sticky="w")
+email = tkinter.Entry(font=("", 15, "normal"))
+email.grid(row=3, column=2, pady=(0, 20))
 
-password_label = tkinter.Label(text="Password: ")
-password_label.pack()
-password = tkinter.Entry()
-password.pack()
+password_label = tkinter.Label(text="Password", bg="#E1E9EF", font=("", 12, "normal"))
+password_label.grid(row=4, column=1, sticky="W")
+password = tkinter.Entry(font=("", 15, "normal"))
+password.grid(row=5, column=1, pady=(0, 12), padx=(0, 50))
 
-save_button = tkinter.Button(text="Save", command=lambda: save.save(platform.get(), email.get(), password.get()))
-save_button.pack()
+save_button = tkinter.Button(text="Save", command=lambda: save.save(platform.get(), email.get(), password.get()), fg="white", bg="#4D7044", font=("", 12, "normal"))
+save_button.grid(row=5, column=2, sticky="ewn")
 
-windows.mainloop()
+window.mainloop()
