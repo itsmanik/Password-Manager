7a46374ac98d695498d644174367bff709e85251
itsmanik <manik.hilalpure@gmail.com>
2024-04-25 11:37:00 +0530

    added feature to load passwords'

--- load.py
+++ load.py
@@ -6,12 +6,14 @@ def load(platform, window):
     platform.delete(0, "end")
 
     with open("data.json") as data:
-        print(json.load(data))
+        saved_passwords = json.load(data)
+
+    mail = saved_passwords[platform_text]["email"]
+    password = saved_passwords[platform_text]["password"]
     
     show_info_window = tkinter.Toplevel(window)
     show_info_window.title("Saved")
     logo = tkinter.PhotoImage(file="logo.png")
-    # show_info_window.minsize(height=350, width=700)
     show_info_window.iconphoto(False, logo)
     show_info_window.config(bg="#E1E9EF", padx=70, pady=30)
 
@@ -20,15 +22,15 @@ def load(platform, window):
 
     platform_label = tkinter.Label(show_info_window, text="Platform: ", bg="#E1E9EF", font=("", 12, "normal")) 
     platform_label.grid(row=2, column=1, sticky="W")
-    platform_saved = tkinter.Label(show_info_window, text="Instagram", bg="#E1E9EF", font=("", 12, "normal")) 
+    platform_saved = tkinter.Label(show_info_window, text=platform_text, bg="#E1E9EF", font=("", 12, "normal")) 
     platform_saved.grid(row=2, column=2, sticky="W")
 
     email_label = tkinter.Label(show_info_window, text="E-Mail: ", bg="#E1E9EF", font=("", 12, "normal")) 
     email_label.grid(row=3, column=1, sticky="W")
-    email_saved = tkinter.Label(show_info_window, text="maniksh2004@gmail.com", bg="#E1E9EF", font=("", 12, "normal")) 
+    email_saved = tkinter.Label(show_info_window, text=mail, bg="#E1E9EF", font=("", 12, "normal")) 
     email_saved.grid(row=3, column=2, sticky="W")
 
     password_label = tkinter.Label(show_info_window, text="Password: ", bg="#E1E9EF", font=("", 12, "normal")) 
     password_label.grid(row=4, column=1, sticky="W")
-    password_saved = tkinter.Label(show_info_window, text="password12345", bg="#E1E9EF", font=("", 12, "normal")) 
+    password_saved = tkinter.Label(show_info_window, text=password, bg="#E1E9EF", font=("", 12, "normal")) 
     password_saved.grid(row=4, column=2, sticky="W")
